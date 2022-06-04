f752d4948590b8340f268772f2962c554a630e0b
eddy <eddy.charenton@epsi.fr>
2022-06-04 09:34:26 +0200

    modif genre.js

--- pages/api/movieGenre/[genre].js
+++ pages/api/movieGenre/[genre].js
@@ -1,14 +1,15 @@
 // pages/api/movieGenre/[genre].js
+import { ObjectId } from "bson";
 import clientPromise from "../../../lib/mongodb";
 // Recherche d'un film avec paramètre : genre
 
 /**
 * @swagger
-* /api/movieGenre/genre:
+* /api/movieGenre/{genre}:
 *   get:
 *       description: Return movie by genre
 *       parameters:
-*            - in: header
+*            - in: path
 *              name: genre
 *              schema:
 *                type: string
@@ -18,12 +19,23 @@ import clientPromise from "../../../lib/mongodb";
 */
 
 export default async function paramsGenre(req, res) {
-    const { genre } = req.headers
+    const { genre } = req.query;
     const client = await clientPromise;
     
     const db = client.db("sample_mflix");
     
-    const movies = await db.collection("movies").find({genres: genre}).toArray();
+    const movies = await db.collection.aggregate([
+        {
+            $match : {
+                genre: genre,
+            },
+        },
+        {
+            $group : {
+                _id: ObjectId(id),
+            },
+        },
+    ])
 
     if(movies){
         if (movies == []){
